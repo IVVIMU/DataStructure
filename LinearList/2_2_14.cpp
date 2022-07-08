86b77612c544743c78d4e86f83780722512d5617
IVVIMU <zfyfishwood@gmail.com>
2022-07-09 00:19:08 +0800

    Exercise Modification

--- LinearList/2_2_14.cpp
+++ LinearList/2_2_14.cpp
@@ -4,6 +4,16 @@
     S2 = {-25, -10, 10, 11}
     S3 = {2, 9, 17, 30, 41}
     最小的三元组为{9, 10, 9}
+
+    算法思想：
+    a------b------c
+    L1 = |a - b|
+    L2 = |b - c|
+    L3 = |c - a|
+    D = L1 + L2 + L3 = 2L3
+    
+    决定D大小的关键是a和c之间的距离，于是问题简化为
+    每次固定c找一个a，使得L3 = |c - a|最小
 */
 
 #include <stdio.h>
@@ -55,4 +65,5 @@ int main() {
     SqList C = {{2, 9, 17, 30, 41}, 5};
     int D_min = findMinOfTrip(A, B, C);
     printf("%d", D_min);
+    return 0;
 }
