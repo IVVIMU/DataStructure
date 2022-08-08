569c80d1c8e463e9e2706e98f069bc5218394df6
IVVIMU <zfyfishwood@gmail.com>
2022-08-09 01:11:08 +0800

    Tree Updating

--- Tree/CSTree.cpp
+++ Tree/CSTree.cpp
@@ -11,3 +11,21 @@ typedef struct CSNode {
     ElemType data;
     struct CSNode *firstchild, *nextsibling;
 } CSNode, *CSTree;
+
+//初始化树的函数，递归方法
+void CreateCSTree(CSTree &T) {
+    ElemType ch;
+    scanf("%d",&ch);
+    //清空缓冲区，输入int型字符时不需要
+    // getchar();
+    if(ch == 0) {
+        //递归结束，建立树
+        T = NULL;  
+    } else{
+        //生成根节点
+        T = (CSNode *) malloc(sizeof(CSNode));
+        T->data = ch;   
+        CreateCSTree(T->firstchild);  //递归创建左子树
+        CreateCSTree(T->nextsibling);  //递归创建右子树
+    }
+}
